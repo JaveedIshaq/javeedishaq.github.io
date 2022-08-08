498ba934a9dc4dc68cde58bdf0de5f288b060d6a
javeedishaq <ishaqjaveed1@gmail.com>
2022-08-08 21:02:41 +0500

    Updates

---
+++ precache-manifest.9ac41ff9bed7e5246adae626bdfcb311.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be272aee6ad03285d4cc4698c6e0c533",
+    "url": "/index.html"
+  },
+  {
+    "revision": "174731f289ec9e6e0f34",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "d5f1cc1684c42b1ae92f",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "e702acc3d76ebd5b81ea",
+    "url": "/static/css/main.1a6005f5.chunk.css"
+  },
+  {
+    "revision": "f3f61ebd928420ffc4b4",
+    "url": "/static/js/2.e9015db5.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.e9015db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "174731f289ec9e6e0f34",
+    "url": "/static/js/3.aecfef34.chunk.js"
+  },
+  {
+    "revision": "d5f1cc1684c42b1ae92f",
+    "url": "/static/js/4.49c75305.chunk.js"
+  },
+  {
+    "revision": "e702acc3d76ebd5b81ea",
+    "url": "/static/js/main.7d16d56b.chunk.js"
+  },
+  {
+    "revision": "2123af63a37770dd8e57",
+    "url": "/static/js/runtime-main.9b56a5dd.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "616b69f7be0171f3320a779bb1904aca",
+    "url": "/static/media/HKUST-original_0.616b69f7.svg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "913adece3d13a4668ff253d8fc055cfd",
+    "url": "/static/media/abcd.913adece.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "208aa41279ba45079f257f75cf89c652",
+    "url": "/static/media/coursera.208aa412.svg"
+  },
+  {
+    "revision": "48fd3e1f087a45ce5d213af7a2a3b06d",
+    "url": "/static/media/daily-expense-manager.48fd3e1f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "6ded58d5ffbc1b36c5b10f3e738d4b05",
+    "url": "/static/media/eeizo.6ded58d5.png"
+  },
+  {
+    "revision": "7fcd0d3b5399dc7ab495882902574a9e",
+    "url": "/static/media/fluttercasa.7fcd0d3b.png"
+  },
+  {
+    "revision": "c8024a2d368e2030aaf3661ea8385726",
+    "url": "/static/media/gtek-logo.c8024a2d.png"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
